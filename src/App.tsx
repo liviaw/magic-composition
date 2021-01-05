bdf1b16af1148590e5e6bd751bc6c195ec168ebe
Livia Wijayanti <livia@canva.com>
2021-01-05 13:55:31 +1100

    hide images

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Player, Home, CreateVideo} from './Components/';
-
+// import styles from './App.css';
 import ReactPlayer, { SourceProps } from 'react-player';
 import { JsxElement } from 'typescript';
 
 const App: React.FC = () => {
   // const [videoFilePath, setVideoPath] = useState<string[] >([]);
   const [files, setFiles] = useState<FileList>();
-  const [media, setMedias] = useState<JSX.Element[]>([]);
+  const [medias, setMedias] = useState<JSX.Element[]>([]);
+  const [imagesCounter, setImagesCounter] = useState(0);
+  let imageFormat = new RegExp('image/*');
+  let videoFormat = new RegExp('video/*');
 
-  const newFun = async (event:React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(()=> {
+    ShowMedia();
+  }, [files])
+  const handleVideoUpload = async (event:React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) return;
     // setVideoPath([ URL.createObjectURL(event.target.files[0])]);
     setFiles(event.target.files)
     console.log(event.target.files);
-  }
-  const handleVideoUpload = async (event:React.ChangeEvent<HTMLInputElement>) => {
-    await newFun(event);
-    console.log(files);
     ShowMedia();
   };
-  
-  // 
+
   const ShowMedia:() => JSX.Element | JSX.Element[]  = () => {
     const mediasTemp:JSX.Element[] = []
     if (files == null) {
@@ -34,15 +35,37 @@ const App: React.FC = () => {
     for (let i = 0; i < files.length; i++) {
       if (imageFormat.test(files[i].type)) {
         console.log(i + files[i].type);
+        // style={{display:"none"}}
         mediasTemp.push(<img src={URL.createObjectURL(files[i])}/>)
       }
       else if (videoFormat.test(files[i].type)) {
         console.log(i + files[i].type);
-        mediasTemp.push(<ReactPlayer url={URL.createObjectURL(files[i])} width="100%" height="50%" playing={true}/>)
+        // style={{display:"none"}} , visibilitiy doesnt work too
+        mediasTemp.push(<ReactPlayer  url={URL.createObjectURL(files[i])} width="100%" height="50%" playing={true}/>)
       }
     }
     setMedias(mediasTemp);
     return mediasTemp;
+  }
+  const idk = async () => {
+    let file = medias[0];
+    // {medias.forEach((file)=>{
+      if (imageFormat.test(file.type)) {
+        await setTimeout(function(){}, 1000); 
+        let temp = medias;
+        temp.splice(0,1);
+        setMedias(temp);
+        console.log(temp);
+
+      }
+    // })}
+  }
+
+  const changeImage = () => {
+    let temp = medias[imagesCounter];
+    // temp.className = styles.show;
+    setImagesCounter(imagesCounter + 1);
+    // visibility=hidden;
   }
 
   return (
@@ -52,7 +75,10 @@ const App: React.FC = () => {
         <input type="submit" value="Submit"></input>
       </form>
       {/* {ShowMedia()} */}
-      {media}
+      {/* {idk()} */}
+      {/* {setInterval(changeImage, 1000)} */}
+      
+      {medias}
 
 
       
