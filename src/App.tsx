f17fed8919b99e74316312b9dc4d2f9566ef4daf
Livia Wijayanti <livia@canva.com>
2021-01-05 12:36:09 +1100

    show media bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,65 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Player, Home, CreateVideo} from './Components/';
-import sampimage1 from './Media/image1.png';
-// import samplevideo1 from './Media/video2.mp4';
 
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import ReactPlayer, { SourceProps } from 'react-player';
+import { JsxElement } from 'typescript';
 
 const App: React.FC = () => {
-  const [videoFilePath, setVideoPath] = useState<string | string[] | SourceProps[] | MediaStream | undefined>("");
-  const handleVideoUpload = (event:React.ChangeEvent<HTMLInputElement>) => {
+  // const [videoFilePath, setVideoPath] = useState<string[] >([]);
+  const [files, setFiles] = useState<FileList>();
+  const [media, setMedias] = useState<JSX.Element[]>([]);
+
+  const newFun = async (event:React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files === null) return;
-    setVideoPath(URL.createObjectURL(event.target.files[0]));
+    // setVideoPath([ URL.createObjectURL(event.target.files[0])]);
+    setFiles(event.target.files)
+    console.log(event.target.files);
+  }
+  const handleVideoUpload = async (event:React.ChangeEvent<HTMLInputElement>) => {
+    await newFun(event);
+    console.log(files);
+    ShowMedia();
   };
+  
+  // 
+  const ShowMedia:() => JSX.Element | JSX.Element[]  = () => {
+    const mediasTemp:JSX.Element[] = []
+    if (files == null) {
+      console.log("empty files");
+      return <></>
+    }
+    let imageFormat = new RegExp('image/*');
+    let videoFormat = new RegExp('video/*');
+    for (let i = 0; i < files.length; i++) {
+      if (imageFormat.test(files[i].type)) {
+        console.log(i + files[i].type);
+        mediasTemp.push(<img src={URL.createObjectURL(files[i])}/>)
+      }
+      else if (videoFormat.test(files[i].type)) {
+        console.log(i + files[i].type);
+        mediasTemp.push(<ReactPlayer url={URL.createObjectURL(files[i])} width="100%" height="50%" playing={true}/>)
+      }
+    }
+    setMedias(mediasTemp);
+    return mediasTemp;
+  }
+
   return (
     <div className="App">
-      <form action="/action_page.php">
-        <input type="file" onChange={handleVideoUpload}/>
+      <form>
+        <input type="file" multiple onChange={handleVideoUpload}/>
         <input type="submit" value="Submit"></input>
       </form>
-      <ReactPlayer url={videoFilePath} width="100%" height="50%" controls={true} playing/>
+      {/* {ShowMedia()} */}
+      {media}
+
+
+      
+      
+      
+      {/* <ReactPlayer url={videoFilePath} width="100%" height="50%" controls={true} playing/> */}
+
       {/* <Player/> */}
 
 
