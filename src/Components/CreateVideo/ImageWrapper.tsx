cd6f215c28770d7cb9dae350db18b43fbd325910
Livia Wijayanti <livia@canva.com>
2021-01-06 16:02:30 +1100

    simple progress bar

--- src/Components/CreateVideo/ImageWrapper.tsx
+++ src/Components/CreateVideo/ImageWrapper.tsx
@@ -2,11 +2,20 @@ import React, { useState, useEffect } from 'react';
 
 type Props = {
     file: File;
+    delay: number;
 }
 
 const ImageWrapper: React.FC<Props> = ({
     file,
+    delay
 }) => {
+    useEffect(() => {
+        setTimeout(imageTimeoutHandler, delay);
+      }, []);
+
+      const imageTimeoutHandler = () => {
+
+      }
     return(<div></div>)
 }
 
