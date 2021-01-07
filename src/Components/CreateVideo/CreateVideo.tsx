558e58c73b6db3adb157d816293b6e95e2b95c51
Livia Wijayanti <livia@canva.com>
2021-01-07 17:46:56 +1100

    fixing progress bar

--- src/Components/CreateVideo/CreateVideo.tsx
+++ src/Components/CreateVideo/CreateVideo.tsx
@@ -1,11 +1,14 @@
 
 
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useInterval } from 'beautiful-react-hooks'; 
 import { Button, Modal } from 'react-bootstrap';
 import styles from './CreateVideo.module.css';
 import VideoProgressBar from './VideoProgressBar';
-import ReactPlayer, { SourceProps } from 'react-player/lazy';
+import ReactPlayer from 'react-player/lazy';
+// import { Player, ControlBar } from 'video-react';
+// import { Slider, Direction } from 'react-player-controls';
+// import {getVideoDurationInSeconds} from 'get-video-duration';
 
 type Props = {
     files:File[];
@@ -22,30 +25,42 @@ const CreateVideo: React.FC<Props> = ({
   }) => {
     const [medias, setMedias] = useState<JSX.Element[]>([]);
     const [mediaCounter, setMediaCounter] = useState<number>(0);
-    const [duration, setDuration] = useState<number>(2000);
-    const [progress, setProgress] = useState<number>(0);
+    const [imageDuration, setImageDuration] = useState<number>(2000);
+    // const [progress, setProgress] = useState<number>(0);
     const [videoPlaying, setVideoPlaying] = useState<boolean>(false);
-    const changeImage = () => {
+    const [videosNum, setVideosNum] = useState<number>(0);
+    const [imagesNum, setImagesNum] = useState<number>(0);
+    const [totalVideoDuration, setTotalVideoDuration] = useState<number>(0);
+    const [player, setPlayer] = useState(null);
+    const changeImage: () => void = () => {
         console.log(mediaCounter);
         if (videoPlaying || files == null) {
             return;
         }
         if (mediaCounter >= files.length) {
-            setProgress(100);
+            // setProgress(100);
             clearInterval();
             console.log("interval cleared");
         } else {
             console.log("videoended");
-            setProgress(progress + 10);
+            // setProgress(progress + 10);
             setMediaCounter(mediaCounter + 1);
         }
     }
-    const [isCleared, clearInterval] = useInterval(changeImage, duration);
+    const [isCleared, clearInterval] = useInterval(changeImage, imageDuration);
     
     useEffect(()=> {
         ShowMedia();
       }, [files])
-
+    // function setFileInfo(file) {
+    //     var video = document.createElement('video');
+    //     video.preload = 'metadata';
+    //     video.src = URL.createObjectURL(file);
+    //     window.URL.revokeObjectURL(video.src);
+    //     return video.duration;
+      
+        
+    //   }
     const ShowMedia:() => JSX.Element | JSX.Element[]  = () => {
         const mediasTemp:JSX.Element[] = []
         if (files == null) {
@@ -55,14 +70,39 @@ const CreateVideo: React.FC<Props> = ({
         let videoFormat = new RegExp('video/*');
         for (let i = 0; i < files.length; i++) {
             if (imageFormat.test(files[i].type)) {
+                setImagesNum(imagesNum + 1);
                 mediasTemp.push(<img className={styles.renderMedia} src={URL.createObjectURL(files[i])}/>)
             }
             else if (videoFormat.test(files[i].type)) {
-            // onEnded={changeImage}
-            mediasTemp.push(<ReactPlayer url={URL.createObjectURL(files[i])} width="100%" height="50%" playing={true} onStart = {() => setVideoPlaying(true)} onEnded={() => setVideoPlaying(false)}/>)
+                setVideosNum(videosNum + 1);
+                let video = new Audio(URL.createObjectURL(files[i]));
+                // let video = document.createElement('video');
+                video.preload = 'metadata';
+                // video.src = URL.createObjectURL(files[i]);
+                window.URL.revokeObjectURL(video.src);
+                setTotalVideoDuration(totalVideoDuration + video.duration);
+                console.log("vid dur is "+ video.duration);
+                mediasTemp.push(<ReactPlayer 
+                    url={URL.createObjectURL(files[i])} 
+                    width="100%" height="50%" 
+                    playing={true} 
+                    onStart = {() => setVideoPlaying(true)} 
+                    onEnded={() => setVideoPlaying(false)} 
+                    onError={()=> alert(files[i] + " is unable to play")}
+                    id={files[i].name}
+                    // onDuration={(duration) => {
+                    //     console.log("duration= "+duration);
+                    //     setTotalVideoDuration(totalVideoDuration + duration)}
+                    // }
+                />)
+                // getVideoDurationInSeconds('../../../Media/video2.mp4').then((duration:number) => {
+                //     setTotalVideoDuration(totalVideoDuration + duration);
+                // });
+
             }
         }
         setMedias(mediasTemp);
+        setTotalVideoDuration(totalVideoDuration + imagesNum*imageDuration);
         return mediasTemp;
     }
       
@@ -74,10 +114,9 @@ const CreateVideo: React.FC<Props> = ({
         <Modal.Body>
             some text here
             <div className={styles.renderMediaContainer}>
-
                 {medias[mediaCounter]}
             </div>
-            {/* <VideoProgressBar file={files[mediaCounter]} progress={progress}/> */}
+            <VideoProgressBar totalVideoDuration={totalVideoDuration}/>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="outline-dark" onClick={handleClose}>
--- src/Components/CreateVideo/VideoProgressBar.tsx
+++ src/Components/CreateVideo/VideoProgressBar.tsx
@@ -1,19 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import { ProgressBar } from 'react-bootstrap';
+import { useInterval } from 'beautiful-react-hooks'; 
 
 type Props = {
-    file: File;
-    progress: number;
+    totalVideoDuration: number;
 }
 
 const VideoProgressBar: React.FC<Props> = ({
-    file,
-    progress,
+    totalVideoDuration,
 }) => {
-    
-    return(<div>
-        <ProgressBar now={progress} label={`${progress}%`} />
-    </div>)
+    const [progressInterval, setProgressInterval] = useState<number>(1000);
+    const [progress, setProgress] = useState<number>(0);
+    const updateProgress = () => {
+        if (progress >= totalVideoDuration) {
+            // video finishes
+            setProgress(100);
+            clearInterval();
+        } else {
+            // updates progress
+            console.log(totalVideoDuration+" microsecond has passed progress= " + progress + " of "+ totalVideoDuration);
+            setProgress(progress + progressInterval);
+        }
+    }
+    console.log("totalVideoDuration= " + totalVideoDuration);
+    const [isCleared, clearInterval] = useInterval(updateProgress, progressInterval);
+    return(
+        <div>
+            <ProgressBar now={progress/totalVideoDuration * 100} label={`${progress/totalVideoDuration * 100}%`} />
+        </div>
+    )
 }
 
 export default VideoProgressBar;
