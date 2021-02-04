dbe5f58c95ce47d9f310da51a1c4a3ffb643709e
Livia Wijayanti <livia@canva.com>
2021-02-04 17:20:55 +1100

    remove css file of video modal

--- src/Components/VideoModal/VideoPlayer.tsx
+++ src/Components/VideoModal/VideoPlayer.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from "react";
-// import { VideoProgressBar } from "./VideoProgressBar";
 import styles from "./VideoModal.module.css";
 import { MediaComponent } from "./MediaComponent";
-// import RotateLoader from "react-spinners/RotateLoader";
 import { MediaPresenter } from "../MediaPresenter";
 import { observer } from "mobx-react";
-// import ReactPlayer from "react-player";
 import playButton from "./playButton.png";
 import pauseButton from "./pauseButton.png";
 import replayButton from "./replayButton.png";
@@ -30,7 +27,7 @@ export const VideoPlayer: React.FC<Props> = observer(
     const onEnded = (playedDur: number): void => {
       console.log(mediaCounter);
       // // if files are not attached or if video is playing, do not change interval
-      if ( filesLength === 0) {
+      if (filesLength === 0) {
         return;
       }
       // TODO: if too many media or too little slots, manage it.
@@ -38,7 +35,6 @@ export const VideoPlayer: React.FC<Props> = observer(
       if (music.ended) {
         // should never go here? Because I set up the templates to be short or equal to music lengtj
         setMediaCounter(filesLen - 1);
-        console.log("music has ended");
       } else if (slot.slot.length <= mediaCounter) {
         setMediaCounter(filesLen - 1);
         music.pause();
@@ -50,37 +46,34 @@ export const VideoPlayer: React.FC<Props> = observer(
         setMediaCounter(mediaCounter + 1);
       }
     };
-   
 
-     // if the video is the last file, lower the volume of music
-     if (music.currentTime >= slot.end - 3) {
-       console.log(" slot.end is " +slot.end);
-       console.log("music current time " + music.currentTime);
-       console.log( " seconds left")
-       console.log(slot.end - music.currentTime);
+    // if the video is the last file, lower the volume of music
+    if (music.currentTime >= slot.end - 3) {
+      console.log(slot.end - music.currentTime);
       if (music.volume - 0.1 > 0) {
-        console.log("before " + music.volume);
         music.volume -= 0.1;
-        console.log("after " + music.volume);
       }
     }
 
-    console.log("videplayer " + mediaPresenter.getDuration(mediaCounter, styleIndex));
+    console.log(
+      "videplayer " + mediaPresenter.getDuration(mediaCounter, styleIndex)
+    );
 
     return (
       <div style={{ display: "flex", flexDirection: "column" }}>
-       { mediaCounter < mediaPresenter.filesLength && 
-        <MediaComponent
-          file={mediaPresenter.getFile(mediaCounter, styleIndex)}
-          onEnded={onEnded}
-          interval={slot.slot[mediaCounter]}
-          mediaDur={mediaPresenter.getDuration(mediaCounter, styleIndex)}
-          play={play}
-          setPlay={setPlay}
-          playfrom={mediaPresenter.getFilePlayed(mediaCounter, styleIndex)}
-          //  idt this is the right filename
-          key={mediaPresenter.getFileName(mediaCounter)}
-        />}
+        {mediaCounter < mediaPresenter.filesLength && (
+          <MediaComponent
+            file={mediaPresenter.getFile(mediaCounter, styleIndex)}
+            onEnded={onEnded}
+            interval={slot.slot[mediaCounter]}
+            mediaDur={mediaPresenter.getDuration(mediaCounter, styleIndex)}
+            play={play}
+            setPlay={setPlay}
+            playfrom={mediaPresenter.getFilePlayed(mediaCounter, styleIndex)}
+            //  idt this is the right filename
+            key={mediaPresenter.getFileName(mediaCounter)}
+          />
+        )}
         {play ? (
           <img
             className={styles.pauseButton}
@@ -95,8 +88,8 @@ export const VideoPlayer: React.FC<Props> = observer(
           <img
             onClick={() => {
               // music.currentTime=slot.start;
-                music.play();
-                setPlay(true);
+              music.play();
+              setPlay(true);
             }}
             className={styles.playButton}
             src={playButton}
@@ -104,21 +97,20 @@ export const VideoPlayer: React.FC<Props> = observer(
           />
         )}
 
-        {
-          (mediaCounter >= mediaPresenter.filesLength || music.paused )&&
-          (<img
+        {(mediaCounter >= mediaPresenter.filesLength || music.paused) && (
+          <img
             onClick={() => {
               setMediaCounter(0);
               music.load();
-              music.currentTime=slot.start;
+              music.currentTime = slot.start;
               music.play();
               setPlay(true);
             }}
             className={styles.replayButton}
             src={replayButton}
             alt="replay button"
-          />) 
-        }
+          />
+        )}
       </div>
     );
   }
