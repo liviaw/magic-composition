ccc47f42eb5a7fbc98042866d28b7b86fbc96e61
Livia Wijayanti <livia@canva.com>
2021-02-05 17:28:49 +1100

    Revert "Revert "Moving media presenter out of app""

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { MediaPresenter } from "./Components/MediaPresenter";
+
+const mediaPresenter = new MediaPresenter();
 
 ReactDOM.render(
-    <App />,
+    <App mediaPresenter={mediaPresenter}/>,
 
   document.getElementById('root')
 );
