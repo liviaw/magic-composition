93115522c3b72d9c462564785ada20eaeb0a8931
Livia Wijayanti <livia@canva.com>
2021-02-08 17:02:35 +1100

    passing in closemodal

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
 import type { MediaPresenter } from "../MediaPresenter";
-import { Button, Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 import { ErrorToast } from "../ErrorToast/ErrorToast";
 import styles from "./Modal.module.css";
 import classnames from "classnames";
 type Props = {
   mediaPresenter: MediaPresenter;
   modalOpen: boolean;
-  setModalOpen: (value: boolean) => void;
+  closeModal: () => void;
 };
 
 export const Modal: React.FC<Props> = observer(
-  ({ mediaPresenter, modalOpen, setModalOpen }) => {
-    const closeModal = (): void => {
-      setModalOpen(false);
-    };
+  ({ mediaPresenter, modalOpen, closeModal}) => {
     const windowModal = (
       event: React.MouseEvent<HTMLDivElement, MouseEvent>
     ): void => {
@@ -29,9 +26,7 @@ export const Modal: React.FC<Props> = observer(
         <Container
           fluid="sm"
           className={styles.modalContent}
-          onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-            windowModal(event);
-          }}
+          onClick={windowModal}
         >
           <ErrorToast />
           <span className={styles.close} onClick={closeModal}>
@@ -42,9 +37,11 @@ export const Modal: React.FC<Props> = observer(
               <Row className={styles.modalHeader}>
                 <h3>Create a Video</h3>
               </Row>
-              <Row className={styles.steps}></Row>
+              <Row className={styles.steps}>
+              </Row>
             </Col>
-            <Col className={styles.modalBody} sm={8}></Col>
+            <Col className={styles.modalBody} sm={8}>
+            </Col>
           </Row>
         </Container>
       </div>
