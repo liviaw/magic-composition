e324f5079dfd0b69e366d18f0f112fbf808deeb1
Livia Wijayanti <livia@canva.com>
2021-02-08 13:09:48 +1100

    add modal state

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,24 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { observer } from "mobx-react";
 import { Button, Container, Col, Row } from "react-bootstrap";
-import styles from "./Modal.module.css";
+import { ErrorToast } from "../ErrorToast/ErrorToast";
 import type { MediaPresenter } from "../MediaPresenter";
-
+import styles from "./Modal.module.css";
+import classnames from "classnames";
 type Props = {
   mediaPresenter: MediaPresenter;
 };
 
 export const Modal: React.FC<Props> = observer(({ mediaPresenter }) => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const openModal = (): void => {
-    var modal: HTMLElement | null = document.getElementById("magicModal");
-    if (modal != null) {
-      modal.style.display = "block";
-    }
+    setModalOpen(true);
   };
   const closeModal = (): void => {
     var modal: HTMLElement | null = document.getElementById("magicModal");
     if (modal != null) {
-      modal.style.display = "none";
+      // modal.style.display = "none";
+      setModalOpen(false);
     }
   };
   const windowCloseModal = (
@@ -26,44 +26,41 @@ export const Modal: React.FC<Props> = observer(({ mediaPresenter }) => {
   ): void => {
     var modal: HTMLElement | null = document.getElementById("magicModal");
     if (event.target === modal && modal != null) {
-      modal.style.display = "none";
+      // modal.style.display = "none";
+      setModalOpen(false);
     }
   };
   return (
     <div>
       <Button
         variant="info"
-        className={styles.createDesignBtn}
         onClick={openModal}
+        className={styles.createDesignBtn}
       >
         Create a design
       </Button>
 
       <div
         id="magicModal"
-        className={styles.modal}
+        // className={modalOpen? styles.modalOpen : styles.modalClose }
+        className={classnames(styles.modal, { [styles.open]: modalOpen })}
         onClick={(event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
           windowCloseModal(event);
         }}
       >
         <Container fluid="sm" className={styles.modalContent}>
+          <ErrorToast />
           <span className={styles.close} onClick={closeModal}>
             &times;
           </span>
           <Row className={styles.leftModal}>
             <Col sm={4}>
               <Row className={styles.modalHeader}>
-                <h2>Create a Video</h2>
+                <h3>Create a Video</h3>
               </Row>
-              <Row className={styles.steps}>steps</Row>
+              <Row className={styles.steps}></Row>
             </Col>
-            <Col className={styles.modalBody} sm={8}>
-              {mediaPresenter.filesLength === 0 ? (
-                <div>show drop box</div>
-              ) : (
-                <div>show player</div>
-              )}
-            </Col>
+            <Col className={styles.modalBody} sm={8}></Col>
           </Row>
         </Container>
       </div>
