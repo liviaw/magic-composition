da1329f163db8098be436446310e2ecbc449584d
Livia Wijayanti <livia@canva.com>
2021-02-12 13:45:52 +1100

    add drop down into homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React, { useState } from "react";
 import "./App.css";
-import type { MediaPresenter } from "./Components/MediaPresenter";
+import type { MediaPresenter } from "./MediaPresenter";
+import type { OutputPresenter } from "./OutputPresenter";
 import { Modal } from "./Components/Modal/Modal";
 import { Homepage } from "./Components/Homepage/Homepage";
+
 type AppProps = {
   mediaPresenter: MediaPresenter;
+  outputPresenter: OutputPresenter;
 };
 
-const App: React.FC<AppProps> = ({mediaPresenter}) => { 
+const App: React.FC<AppProps> = ({mediaPresenter, outputPresenter}) => { 
   const [modalOpen, setModalOpen] = useState<boolean>(false);
   const closeModal = () => {
     setModalOpen(false);
+    outputPresenter.pauseVideo();
   }
 
-  const openModal = ()  => {
+  const openModal = (event: React.MouseEvent<HTMLDivElement, MouseEvent>)  => {
     setModalOpen(true);
+    if (mediaPresenter.mediaReady) {
+      outputPresenter.playVideo();
+    }
+    event.stopPropagation();
   }
   return (
     <div className="App">
       <Homepage openModal={openModal}/>
-      <Modal mediaPresenter={mediaPresenter} modalOpen={modalOpen} closeModal={closeModal}/>
+      <Modal mediaPresenter={mediaPresenter} outputPresenter={outputPresenter} modalOpen={modalOpen} closeModal={closeModal}/>
+      {outputPresenter.playingMedia}
     </div>
   );
 };
