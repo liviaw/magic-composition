bc56c79f07a34a1117821f6105284e340eef759b
Livia Wijayanti <livia@canva.com>
2021-02-16 16:40:44 +1100

    add music name test

--- src/tests/OutputPresenter.test.ts
+++ src/tests/OutputPresenter.test.ts
@@ -7,7 +7,6 @@ describe("output presenter", () => {
   window.HTMLMediaElement.prototype.play = jest.fn();
   window.HTMLMediaElement.prototype.pause = jest.fn();
   window.HTMLMediaElement.prototype.load = jest.fn();
-  const adjustSound = jest.fn();
   beforeEach(() => {
     presenter = new OutputPresenter();
   });
@@ -23,7 +22,6 @@ describe("output presenter", () => {
     });
 
     it("should set play as true", () => {
-      presenter.play = false;
       presenter.playVideo();
       expect(presenter.play).toBe(true);
     });
@@ -31,15 +29,9 @@ describe("output presenter", () => {
 
   describe("pauseVideo", () => {
     it("should set play as false", () => {
-      presenter.play = true;
       presenter.pauseVideo();
       expect(presenter.play).toBe(false);
     });
-
-    it("should pause music", () => {
-      presenter.pauseVideo();
-      expect(presenter.music.pause).toHaveBeenCalled();
-    })
   });
 
   describe("setPlayingMedia", () => {
@@ -49,33 +41,11 @@ describe("output presenter", () => {
     });
   });
 
-  describe("incrementPlayedSeconds", () => {
-    it("should increase the value of playedMedia by 1 if playedseconds is longer than given slot", () => {
+  describe("incrementPlayingMedia", () => {
+    it("should increase the value of playingMedia by 1", () => {
       expect(presenter.playingMedia).toEqual(0);
-      presenter.playedSeconds = 100;
-      presenter.currLength.slot[0] = 5;
-      presenter.incrementPlayedSeconds(0.1);
+      presenter.incrementPlayingMedia();
       expect(presenter.playingMedia).toEqual(1);
-    });
-
-    it("should increase the value of playedSeconds by 1 by the passed in seconds", () => {
-      expect(presenter.playedSeconds).toEqual(0);
-      presenter.playedSeconds = 0;
-      presenter.currLength.slot[0] = 100;
-      presenter.incrementPlayedSeconds(0.1);
-      expect(presenter.playedSeconds).toEqual(0.1);
-    });
-
-    it("should increase the value of overallPlayedSeconds by 1 by the passed in seconds", () => {
-      expect(presenter.overallPlayedSeconds).toEqual(0);
-      presenter.incrementPlayedSeconds(0.1);
-      expect(presenter.overallPlayedSeconds).toEqual(0.1);
-    });
-
-    it("should pause if overall played second is greater than total video duration", () => {
-      presenter.overallPlayedSeconds = 1000;
-      presenter.setCurrTrack(0, 2);
-      expect(presenter.music.pause).toHaveBeenCalled();
     });
   });
 
@@ -85,7 +55,7 @@ describe("output presenter", () => {
       presenter.music.currentTime = 100;
       presenter.currLength.end = 0;
       presenter.adjustSound();
-      expect(presenter.music.volume ).toEqual(0.49);
+      expect(presenter.music.volume).toEqual(0.49);
     });
 
     it("should increase the volume by 0.01 given currentTime is less than or equal (startofmusic + VOLUMEPOINT)", () => {
@@ -93,16 +63,16 @@ describe("output presenter", () => {
       presenter.music.currentTime = 0;
       presenter.currLength.start = 100;
       presenter.adjustSound();
-      expect(presenter.music.volume ).toEqual(0.51);
+      expect(presenter.music.volume).toEqual(0.51);
     });
-    
+
     it("should not change the volume in between the video", () => {
       presenter.music.volume = 0.5;
       presenter.music.currentTime = 250;
       presenter.currLength.start = 0;
       presenter.currLength.end = 500;
       presenter.adjustSound();
-      expect(presenter.music.volume ).toEqual(0.5);
+      expect(presenter.music.volume).toEqual(0.5);
     });
   });
 
@@ -111,6 +81,14 @@ describe("output presenter", () => {
       expect(presenter.playingMedia).toEqual(0);
       presenter.incrementPlayingMedia();
       expect(presenter.playingMedia).toEqual(1);
+    });
+  });
+
+  describe("musicName", () => {
+    it("should append music name with ... if it is too long to display", () => {
+      presenter.currTrack.musicName =
+        "this is a very very long music track name";
+      expect(presenter.musicName).toContain("...");
     });
   });
 });
